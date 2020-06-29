38af3566323ad27e8bc9c776bd5599701749d527
Laxman007 <lakshmangoyalladwa@gmail.com>
2020-06-29 23:39:48 +0530

    Setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit',(e)=>{
     msg1.textContent="Loading"
     msg2.textContent=""
     //console.log(location)
-    const url='http://localhost:3000/weather?address='+location
+    const url='/weather?address='+encodeURLComponent(location)
     fetch(url).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ const express=require('express')
 const path=require('path')
 const hbs=require('hbs')
 const forecast=require('./utils/forecast')
+
 const app=express()
+
+const port=process.env.PORT || 3000
 
 // ? Define paths for express config 
 const publicDirectory=path.join(__dirname,'../public')
@@ -66,6 +69,6 @@ app.get('*',(req,res)=>{
 })
 
 // ? Start the server to listen the requests
-app.listen(3000,()=>{
-    console.log('Server started at 3000')
+app.listen(port,()=>{
+    console.log('Server started at '+port)
 })
